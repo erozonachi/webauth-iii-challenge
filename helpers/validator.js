9730a9d33eacbf46f2330e920213d3f270ba1027
erozonachi <erozonachi.james@gmail.com>
2019-07-24 14:53:32 +0100

    Add validate-user function to the validator

--- helpers/validator.js
+++ helpers/validator.js
@@ -1,3 +1,26 @@
 module.exports = {
-  
+  validateUser: function(req, res, next) {
+    const { username, department, password } = req.body;
+
+    if(!username || username.trim('') === '') {
+      return res.status(400)
+        .json({
+          message: 'missing required username field'
+        });
+    }
+    if(!department || department.trim('') === '') {
+      return res.status(400)
+        .json({
+          message: 'missing required department field'
+        });
+    }
+    if(!password || password.trim('') === '') {
+      return res.status(400)
+        .json({
+          message: 'missing required password field'
+        });
+    }
+
+    next();
+  }
 }
