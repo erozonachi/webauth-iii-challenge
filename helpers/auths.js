f225cdc64cfa15b2c6ddb6298adc852ea873474a
erozonachi <erozonachi.james@gmail.com>
2019-07-24 15:40:40 +0100

    Fix secret usage bug in auths helper module

--- helpers/auths.js
+++ helpers/auths.js
@@ -1,10 +1,10 @@
 const jwt = require('jsonwebtoken');
-const secret = require('../config/secret');
+const secret = require('../config/secret').jwtSecret;
 
 module.exports = {
   authorize: function(req, res, next) {
     const { authorization } = req.headers;
-    const payload = jwt.verify(authorization, secret);
+    const payload = authorization? jwt.verify(authorization, secret) : null;
     if(payload && payload.sub) {
       req.user = payload;
       next();
