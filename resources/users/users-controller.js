4f06cc16ecb317fb01482eeee156eda1418c2902
erozonachi <erozonachi.james@gmail.com>
2019-07-24 13:19:13 +0100

    Add create function to the users-controller

--- resources/users/users-controller.js
+++ resources/users/users-controller.js
@@ -2,5 +2,17 @@ const usersModel = require('./users-model');
 const bcrypt = require('bcryptjs');
 
 module.exports = {
-  
+  create: async function(req, res, next) {
+    try {
+      const user = await usersModel.create({
+        ...req.body,
+        password: bcrypt.hashSync(req.body.password, 12)
+      });
+
+      res.status(201)
+        .json(user);
+    } catch(error) {
+      next(error);
+    }
+  },
 };
