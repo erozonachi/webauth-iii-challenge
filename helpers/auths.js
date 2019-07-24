da76242749ccfc4c3ac8eaeb892e47994d65796b
erozonachi <erozonachi.james@gmail.com>
2019-07-24 15:03:57 +0100

    Add authorize function to the auths module

--- helpers/auths.js
+++ helpers/auths.js
@@ -2,5 +2,17 @@ const jwt = require('jsonwebtoken');
 const secret = require('../config/secret');
 
 module.exports = {
-  
+  authorize: function(req, res, next) {
+    const { authorization } = req.headers;
+    const payload = jwt.verify(authorization, secret);
+    if(payload && payload.sub) {
+      req.user = payload;
+      next();
+    } else {
+      res.status(401)
+        .json({
+          message: 'unauthorized access'
+        });
+    }
+  }
 }
